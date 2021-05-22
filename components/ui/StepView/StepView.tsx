ba1c402f496f32084cc6a6b6989e811310081549
yotaiyo <yota071218@gmail.com>
2021-05-22 23:42:12 +0900

    save

--- components/ui/StepView/StepView.tsx
+++ components/ui/StepView/StepView.tsx
@@ -1,25 +1,63 @@
-import { VStack, HStack, Text, Flex } from '@chakra-ui/react'
+import { VStack, HStack, Text, Flex, Spacer } from '@chakra-ui/react'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+
 import { Heading } from '../Heading'
 
 export type StepViewProps = {
   title: string
   currentStep: number
   numOfStep: number
+  previousTitle?: string
+  nextTitle?: string
+  onClickPrevious?: () => void
+  onClickNext?: () => void
 }
 
-export const StepView: React.FC<StepViewProps> = ({ title, currentStep, numOfStep, children }) => {
+export const StepView: React.FC<StepViewProps> = ({
+  title,
+  currentStep,
+  numOfStep,
+  previousTitle,
+  nextTitle,
+  onClickPrevious,
+  onClickNext,
+  children
+}) => {
   return (
-    <VStack spacing='48px' align='left'>
-      <VStack align='left'>
-        <HStack>
-          <Heading variant='screenTitle'>{currentStep}</Heading>
-          <Flex height='40px'>
-            <Text color='gray.500' alignSelf='flex-end'>{`/ ${numOfStep}`}</Text>
-          </Flex>
-        </HStack>
-        <Heading variant='sectionTitle'>{title}</Heading>
+    <VStack align='left'>
+      <VStack spacing='48px' align='left' width='60%'>
+        <VStack align='left'>
+          <HStack>
+            <Heading variant='screenTitle'>{currentStep}</Heading>
+            <Flex height='40px'>
+              <Text color='gray.500' alignSelf='flex-end'>{`/ ${numOfStep}`}</Text>
+            </Flex>
+          </HStack>
+          <Heading variant='sectionTitle'>{title}</Heading>
+        </VStack>
+
+        {children}
+
+        <Flex>
+          <VStack align='left' onClick={onClickPrevious} style={{ cursor: 'pointer' }}>
+            <HStack>
+              <ArrowBackIcon />
+              <Text>previous</Text>
+            </HStack>
+            <Text>{previousTitle}</Text>
+          </VStack>
+
+          <Spacer />
+
+          <VStack  onClick={onClickNext} style={{ cursor: 'pointer' }}>
+            <HStack>
+              <Text>next</Text>
+              <ArrowForwardIcon />
+            </HStack>
+            <Text>{nextTitle}</Text>
+          </VStack>
+        </Flex>
       </VStack>
-      {children}
     </VStack>
   )
 }
