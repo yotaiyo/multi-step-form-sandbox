6eec7ea3acca6a5d3574943361c82ff01160d762
yotaiyo <yota071218@gmail.com>
2021-05-30 20:41:38 +0900

    Add pagination

---
+++ components/ui/Pagination/Pagination.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Box } from '@chakra-ui/react'
+
+import { Pagination, PaginationProps } from './Pagination';
+
+export default {
+  title: 'Example/Pagination',
+  component: Pagination,
+} as Meta;
+
+const Template: Story<PaginationProps> = (args) => {
+  return (
+    <Box width='512px'>
+      <Pagination {...args} />
+    </Box>
+  )
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  previousTitle: 'previous',
+  nextTitle: 'nextTitle'
+};
---
+++ components/ui/Pagination/Pagination.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Flex, VStack, Text, HStack, Spacer } from '@chakra-ui/react'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+
+export type PaginationProps = {
+  previousTitle?: string
+  nextTitle?: string
+  onClickPrevious?: () => void
+  onClickNext?: () => void
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  previousTitle,
+  nextTitle,
+  onClickPrevious,
+  onClickNext
+}) => {
+  return (
+    <Flex>
+      {previousTitle &&
+        <VStack alignItems='flex-start' onClick={onClickPrevious} style={{ cursor: 'pointer' }}>
+          <Text>previous</Text>
+          <HStack>
+            <ArrowBackIcon boxSize='6' color='orange.500' />
+            <Text fontSize='large' color='orange.500' fontWeight='600'>{previousTitle}</Text>
+          </HStack>
+        </VStack>
+      }
+
+      <Spacer />
+
+      {
+        nextTitle &&
+          <VStack alignItems='flex-end' onClick={onClickNext} style={{ cursor: 'pointer' }}>
+            <Text>next</Text>
+            <HStack>
+              <Text fontSize='large' color='blue.500' fontWeight='600'>{nextTitle}</Text>
+              <ArrowForwardIcon boxSize='6' color='blue.500' />
+            </HStack>
+          </VStack>
+      }
+    </Flex>
+  )
+}
